d611114c8c697559930438a60c63b352eef47388
Nguyễn Văn Hồng <70307363+sitpro993@users.noreply.github.com>
2021-10-27 11:25:37 +0700

    push

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+  introTitle: "Hee luu cậu!",
+  introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+  btnIntro: "Oke",
+  title: "Cậu yêu tớ có phải không :3",
+  desc: "Cậu không trả lời hoặc thoát ra, tức cậu là vợ tớ đó nha :>",
+  btnYes: "yêu ơi là iuuu <33",
+  btnNo: "cậu mơ à :v",
+  question: "Nói cho tớ biết lí do cậu thích tớ đii <3",
+  btnReply: "Gửi cho tớ <3",
+  reply: "Tại vì cậu quá đẹp trai <33",
+  mess: "Tớ biết mà :>. I love you 3000 <3",
+  messDesc: "còn chần chờ gì nữa mà không inbox cho mình",
+  btnAccept: "Yêu cậu :3",
+  messLink: "messLink", //link fb
+  bgLink: "./img/bg.png", //link ảnh nền
+  iputBgLink: "./img/iput-bg.png", //link iputBgLink
+  lookMeLink: "./img/lookMe.png", //link ảnh lookMe
+  soundLink: "./img/soundLink.mp3", //link nhac
+  duckLink: "./img/duckLink.mp3", //link aam thanh 1
+  swishLink: "./img/swishLink.mp3", //link swish
+};
